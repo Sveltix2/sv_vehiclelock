50846512706edfb60b3a9c8f162100035c8c6b0a
Sveltix2 <96305238+Sveltix2@users.noreply.github.com>
2022-06-27 21:50:38 +0200

    Add files via upload

---
+++ chiudi_veicolo/client.lua
@@ -0,0 +1,103 @@
+local ESX = exports['es_extended']:getSharedObject()
+
+local isRunningWorkaround = false
+
+function StartWorkaroundTask()
+	if isRunningWorkaround then
+		return
+	end
+
+	local timer = 0
+	local playerPed = PlayerPedId()
+	isRunningWorkaround = true
+
+	while timer < 100 do
+		Citizen.Wait(0)
+		timer = timer + 1
+
+		local vehicle = GetVehiclePedIsTryingToEnter(playerPed)
+
+		if DoesEntityExist(vehicle) then
+			local lockStatus = GetVehicleDoorLockStatus(vehicle)
+
+			if lockStatus == 4 then
+				ClearPedTasks(playerPed)
+			end
+		end
+	end
+
+	isRunningWorkaround = false
+end
+
+local targadue = nil
+local veicoli = {}
+RegisterNetEvent("veicololock")
+AddEventHandler("veicololock", function(targa)
+	table.insert(veicoli, {targad = targa})
+end)
+
+local ebrei = "anim@mp_player_intmenu@key_fob@"
+
+function chiudi()
+	local playerPed = PlayerPedId()
+	local coords = GetEntityCoords(playerPed)
+	local vehicle
+
+	Citizen.CreateThread(function()
+		StartWorkaroundTask()
+	end)
+
+	if IsPedInAnyVehicle(playerPed, false) then
+		vehicle = GetVehiclePedIsIn(playerPed, false)
+	else
+		vehicle = GetClosestVehicle(coords, 8.0, 0, 71)
+	end
+
+	if not DoesEntityExist(vehicle) then
+		return
+	end
+
+	ESX.TriggerServerCallback('sv_richiedi:veicoli', function(t)
+
+		for k,v in pairs(veicoli) do
+			if GetVehicleNumberPlateText(vehicle) == v.targad then
+				targadue = v.targad
+			else
+				targadue = nil
+			end
+		end
+
+		if t or GetVehicleNumberPlateText(vehicle) == targadue then
+			--
+			RequestAnimDict(ebrei)
+			while not HasAnimDictLoaded(ebrei) do
+				Citizen.Wait(0)
+			end
+			--
+			local lockStatus = GetVehicleDoorLockStatus(vehicle)
+
+			if lockStatus == 1 then 
+				SetVehicleDoorsLocked(vehicle, 2)
+				PlayVehicleDoorCloseSound(vehicle, 1)
+
+				TaskPlayAnim(PlayerPedId(), ebrei, "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
+
+				ESX.ShowNotification('Veicolo chiuso')
+			elseif lockStatus == 2 then 
+				SetVehicleDoorsLocked(vehicle, 1)
+				PlayVehicleDoorOpenSound(vehicle, 0)
+
+				TaskPlayAnim(PlayerPedId(), ebrei, "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
+
+				ESX.ShowNotification("Veicolo aperto")
+			end
+		end
+
+	end, ESX.Math.Trim(GetVehicleNumberPlateText(vehicle)))
+end
+
+RegisterKeyMapping('+chiudi_veh', 'Chiudi Veicolo', 'keyboard', "U")
+
+RegisterCommand('+chiudi_veh', function()
+	chiudi()
+end)
---
+++ chiudi_veicolo/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Sistema chiusura veicoli [personali/lavorativi}'
+
+version '1.0.0'
+
+server_script {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
